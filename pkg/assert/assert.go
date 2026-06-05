f67d0f0cf00ac23680f3fdae7807bb5d3e13ead3
Clevanilson <clevanilson.contato@gmail.com>
2026-06-05 19:49:28 -0300

    test: add `assert` util with `Equal` method

---
+++ pkg/assert/assert.go
@@ -0,0 +1,9 @@
+package assert
+
+import "testing"
+
+func Equal[TValue comparable](t *testing.T, value1, value2 TValue) {
+	if value1 != value2 {
+		t.Fatalf("Expected: %v to equals %v", value1, value2)
+	}
+}
