d0726c230b3a2e0ba60eac8b734ece70de69ed5f
Clevanilson <clevanilson.contato@gmail.com>
2026-06-06 16:12:42 -0300

    test: improve test fail visualization

--- pkg/assert/assert.go
+++ pkg/assert/assert.go
@@ -4,6 +4,6 @@ import "testing"
 
 func Equal[TValue comparable](t *testing.T, value1, value2 TValue) {
 	if value1 != value2 {
-		t.Fatalf("Expected: %v to equals %v", value1, value2)
+		t.Fatalf("🔴 Expected: %v to equals %v", value1, value2)
 	}
 }
