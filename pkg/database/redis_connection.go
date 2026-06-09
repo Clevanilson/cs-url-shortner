e6f764cf2c2ddeb3744aba77dbca8480481474cf
Clevanilson <clevanilson.contato@gmail.com>
2026-06-09 20:05:14 -0300

    style: rename `RedisConnection`

--- pkg/database/redis_connection.go
+++ pkg/database/redis_connection.go
@@ -8,28 +8,35 @@ import (
 
 var ctx = context.Background()
 
-type RedisConnection struct {
+type RedisCoonection interface {
+	Set(key, value string) error
+	Get(key string) (string, error)
+	Remove(key string) error
+	Increment(key string) (int64, error)
+}
+
+type redisConnection struct {
 	client *redis.Client
 }
 
-func NewRedisConnection() *RedisConnection {
+func NewRedisConnection() *redisConnection {
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
 		DB:       0,
 	})
 	client.Set(ctx, "global:id", 999, 0)
-	return &RedisConnection{client}
+	return &redisConnection{client}
 }
 
-func (c *RedisConnection) Set(key, value string) error {
+func (c *redisConnection) Set(key, value string) error {
 	if err := c.client.Set(ctx, key, value, 0).Err(); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (c *RedisConnection) Get(key string) (string, error) {
+func (c *redisConnection) Get(key string) (string, error) {
 	result, err := c.client.Get(ctx, key).Result()
 	if err != nil {
 		return "", nil
@@ -37,7 +44,7 @@ func (c *RedisConnection) Get(key string) (string, error) {
 	return result, nil
 }
 
-func (c *RedisConnection) Increment(key string) (int64, error) {
+func (c *redisConnection) Increment(key string) (int64, error) {
 	result, err := c.client.Incr(ctx, key).Result()
 	if err != nil {
 		return 0, err
@@ -45,7 +52,7 @@ func (c *RedisConnection) Increment(key string) (int64, error) {
 	return result, nil
 }
 
-func (c *RedisConnection) Remove(key string) error {
+func (c *redisConnection) Remove(key string) error {
 	if err := c.client.Del(ctx, key).Err(); err != nil {
 		return err
 	}
